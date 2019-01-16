d53ff74402bd807e4757c094e514202472003626
Bohdan Venhrenovych <bohdanvenhrenovych@gmail.com>
2019-01-16 18:09:41 +0200

    Added git ignore

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 let CURRENCY;
 let AMOUNT_OF_CONTRIBUTION;
-let YEAR_RATE = 0;
-let TERM = 0;
-let TERM_OF_PAYMENT = 0;
-let TYPE_OF_PAYMENT = 'Місяців'; 
+let YEAR_RATE;
+let TERM;
+let TERM_OF_PAYMENT;
+// let TYPE_OF_PAYMENT = 'Місяців'; 
+const URL_CURRENCY = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+
+  
+    $(document).ready(function () {
+      $.getJSON(URL_CURRENCY, 
+        {format: "json"})
+        .done(
+            function(res) {
+                console.log(res);
+            });
+        });
 
 $('.UAN, .USD, .EUR').click(function() {
     let newCurrency = this.innerHTML;
@@ -12,11 +23,11 @@ $('.UAN, .USD, .EUR').click(function() {
     CURRENCY = newCurrency;
 });
 
-$('.month, .years').click(function() {
-    let newTermOfPayment = this.innerHTML;
-    $('#term-of-payment').text(newTermOfPayment);
-    TYPE_OF_PAYMENT = newTermOfPayment;
-});
+// $('.month, .years').click(function() {
+//     let newTermOfPayment = this.innerHTML;
+//     $('#term-of-payment').text(newTermOfPayment);
+//     TYPE_OF_PAYMENT = newTermOfPayment;
+// });
 
 // $('#amongOfContribution').keypress(function() {
 //     // AMOUNT_OF_CONTRIBUTION = this.innerHTML
@@ -36,7 +47,22 @@ function calculate() {
     //             `\n`,TERM, 
     //             `\n`,TERM_OF_PAYMENT, 
     //             `\n`,TYPE_OF_PAYMENT);
-    
+    //console.log(TERM_OF_PAYMENT); 
+    let daysCounter = (date) => Math.ceil(Math.abs(new Date(date).getTime() - new Date().getTime()) / (1000 * 3600 * 24));
+    let numberOfDays = daysCounter(TERM_OF_PAYMENT);
+    console.log(numberOfDays);
+    dayRate(numberOfDays);
 }
+
+function dayRate(days) {
+    let revenueFromPercent = ((10000 / 100) * +YEAR_RATE);
+    let result = +AMOUNT_OF_CONTRIBUTION + (revenueFromPercent / 365) * days;
+    return console.log(result.toFixed(2));
+}
+
+
+
+
+
 
 
