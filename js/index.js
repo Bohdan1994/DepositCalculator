b1405d342682b49ad4516cc2f680d8f1022485f1
Bohdan Venhrenovych <bohdanvenhrenovych@gmail.com>
2019-01-21 21:16:06 +0200

    Refactor

--- js/index.js
+++ js/index.js
@@ -3,36 +3,26 @@ let AMOUNT_OF_CONTRIBUTION;
 let YEAR_RATE;
 let TERM;
 let TERM_OF_PAYMENT;
-// let TYPE_OF_PAYMENT = 'Місяців'; 
+let result;
+let percents = 0;
+const today = new Date();
 const URL_CURRENCY = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
 
-  
-    $(document).ready(function () {
-      $.getJSON(URL_CURRENCY, 
-        {format: "json"})
+$(document).ready(function () {
+    $.getJSON(URL_CURRENCY, {
+            format: "json"
+        })
         .done(
-            function(res) {
+            function (res) {
                 console.log(res);
             });
-        });
-
-$('.UAN, .USD, .EUR').click(function() {
-    let newCurrency = this.innerHTML;
-    $('#currencyDropDownMenu').text(newCurrency);
-    console.log(newCurrency);
-    CURRENCY = newCurrency;
 });
 
-// $('.month, .years').click(function() {
-//     let newTermOfPayment = this.innerHTML;
-//     $('#term-of-payment').text(newTermOfPayment);
-//     TYPE_OF_PAYMENT = newTermOfPayment;
-// });
-
-// $('#amongOfContribution').keypress(function() {
-//     // AMOUNT_OF_CONTRIBUTION = this.innerHTML
-//     console.log(this.value);
-// })
+$('.UAN, .USD, .EUR').click(function () {
+    let newCurrency = this.innerHTML;
+    $('#currencyDropDownMenu').text(newCurrency);
+    CURRENCY = newCurrency;
+});
 
 $('#calculate').click(calculate);
 
@@ -40,29 +30,70 @@ function calculate() {
     AMOUNT_OF_CONTRIBUTION = $("#among-of-contribution").val();
     YEAR_RATE = $('#year-rate').val();
     TERM_OF_PAYMENT = $('#datepicker').val();
-    // TERM = $('#term').val();
-    // console.log(   CURRENCY, 
-    //             `\n`,AMOUNT_OF_CONTRIBUTION,
-    //             `\n`, YEAR_RATE, 
-    //             `\n`,TERM, 
-    //             `\n`,TERM_OF_PAYMENT, 
-    //             `\n`,TYPE_OF_PAYMENT);
-    //console.log(TERM_OF_PAYMENT); 
+
     let daysCounter = (date) => Math.ceil(Math.abs(new Date(date).getTime() - new Date().getTime()) / (1000 * 3600 * 24));
+    let termOfPaymentDate = new Date(TERM_OF_PAYMENT);
     let numberOfDays = daysCounter(TERM_OF_PAYMENT);
     console.log(numberOfDays);
-    dayRate(numberOfDays);
+    getDepositDate(TERM_OF_PAYMENT);
+
+    result = +AMOUNT_OF_CONTRIBUTION;
+
+    for (let i = 0; i < numberOfDays; i++) {
+        percents = getDayRate();
+        result += percents;
+    }
+
+    function getDayRate() {
+        let revenueFromPercent = ((result / 100) * +YEAR_RATE);
+        let dayPercent = (revenueFromPercent / 365);
+        return dayPercent;
+    }
+
+    function getDepositDate(d) {
+        let date = {
+            day : d.split('/')[1],
+            month: d.split('/')[0],
+            year: d.split('/')[2]
+        };
+
+        console.log(date);
+
+    }
+    console.log(percents.toFixed(2), result.toFixed(2));
+    
+    
 }
 
-function dayRate(days) {
-    let revenueFromPercent = ((10000 / 100) * +YEAR_RATE);
-    let result = +AMOUNT_OF_CONTRIBUTION + (revenueFromPercent / 365) * days;
-    return console.log(result.toFixed(2));
+function drawReportContent(data) {
+    // console.log($('.table-content'));
+    console.log(data[0]);
+    for (let i = 0; i < data.length; i++) {
+        $('.table-content').append(`
+   <tr>
+        <td>${data[i].step.date}</td>
+        <td>${data[i].step.amongContribution}</td>
+        <td>${data[i].step.revenuePercent}</td>
+        <td>${data[i].step.capitalization}</td>
+   </tr>`);
+    };
+
 }
+drawReportContent([{
+        step: {
+            date: "vova",
+            amongContribution: 10000,
+            revenuePercent: 120,
+            capitalization: 1200
+        }
+    },
+    {
+        step: {
+            date: "vova",
+            amongContribution: 10000,
+            revenuePercent: 120,
+            capitalization: 1200
+        }
+    }
 
-
-
-
-
-
-
+]);
