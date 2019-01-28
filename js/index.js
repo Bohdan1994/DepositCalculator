a88fe770038719eb7aa341a2c2a5050dd57b6134
Bohdan Venhrenovych <bohdanvenhrenovych@gmail.com>
2019-01-28 19:33:28 +0200

    Rebuilding

--- js/index.js
+++ js/index.js
@@ -5,18 +5,13 @@ let TERM;
 let TERM_OF_PAYMENT;
 let result;
 let percents = 0;
-const today = new Date();
-const URL_CURRENCY = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
-
-$(document).ready(function () {
-    $.getJSON(URL_CURRENCY, {
-            format: "json"
-        })
-        .done(
-            function (res) {
-                console.log(res);
-            });
-});
+// const today = new Date();
+let reportContent = {
+    dateReports: [],
+    amount: [],
+    interestIncone: [],
+    capitalization: []
+};
 
 $('.UAN, .USD, .EUR').click(function () {
     let newCurrency = this.innerHTML;
@@ -26,74 +21,127 @@ $('.UAN, .USD, .EUR').click(function () {
 
 $('#calculate').click(calculate);
 
-function calculate() {
-    AMOUNT_OF_CONTRIBUTION = $("#among-of-contribution").val();
-    YEAR_RATE = $('#year-rate').val();
-    TERM_OF_PAYMENT = $('#datepicker').val();
+class Calculator {
+    constructor(contribution, year_rate, term){
+        let result = 0;
+        this.contr = contribution;
+        this.rate = year_rate;
+        this.term = term;
 
-    let daysCounter = (date) => Math.ceil(Math.abs(new Date(date).getTime() - new Date().getTime()) / (1000 * 3600 * 24));
-    let termOfPaymentDate = new Date(TERM_OF_PAYMENT);
-    let numberOfDays = daysCounter(TERM_OF_PAYMENT);
-    console.log(numberOfDays);
-    getDepositDate(TERM_OF_PAYMENT);
-
-    result = +AMOUNT_OF_CONTRIBUTION;
-
-    for (let i = 0; i < numberOfDays; i++) {
-        percents = getDayRate();
-        result += percents;
-    }
-
-    function getDayRate() {
-        let revenueFromPercent = ((result / 100) * +YEAR_RATE);
-        let dayPercent = (revenueFromPercent / 365);
-        return dayPercent;
-    }
-
-    function getDepositDate(d) {
-        let date = {
-            day : d.split('/')[1],
-            month: d.split('/')[0],
-            year: d.split('/')[2]
+        function daysInMonth(m, y) {
+            return m === 2 ? y & 3 || !(y % 25) && y & 15 ? 28 : 29 : 30 + (m + (m >> 3) & 1);
+         };
+         function calcMonthRevenue() {
+            console.log(this.contr, this.rate, this.term);
         };
 
-        console.log(date);
+      
 
     }
-    console.log(percents.toFixed(2), result.toFixed(2));
-    
-    
-}
-
-function drawReportContent(data) {
-    // console.log($('.table-content'));
-    console.log(data[0]);
-    for (let i = 0; i < data.length; i++) {
-        $('.table-content').append(`
-   <tr>
-        <td>${data[i].step.date}</td>
-        <td>${data[i].step.amongContribution}</td>
-        <td>${data[i].step.revenuePercent}</td>
-        <td>${data[i].step.capitalization}</td>
-   </tr>`);
-    };
-
-}
-drawReportContent([{
-        step: {
-            date: "vova",
-            amongContribution: 10000,
-            revenuePercent: 120,
-            capitalization: 1200
+    numberOfMonth() {
+        function monthDiff(d2, d1) {
+            let months;
+            months = (d2.getFullYear() - d1.getFullYear()) * 12;
+            months -= d1.getMonth() + 1;
+            months += d2.getMonth() + 1;
+            return months <= 0 ? 0 : months;
         }
-    },
-    {
-        step: {
-            date: "vova",
-            amongContribution: 10000,
-            revenuePercent: 120,
-            capitalization: 1200
+
+        function daysInMonth(date) {
+            return new Date(date.getYear(), date.getMonth + 1, 0).getDate();
         }
+
+        function dateDiff(date) {
+            if(date2 && date2.getTime() && !isNaN(date2.getTime())) {
+                
+            }
+         }
+      console.log(dateDiff(this.term));
     }
+     
+    setResult(val) {
+        result += val;
+    }
+}
 
-]);
+    function calculate() {
+    $('.table-content').html(null);
+    AMOUNT_OF_CONTRIBUTION = $("#among-of-contribution").val();
+    YEAR_RATE = $('#year-rate').val();
+    TERM_OF_PAYMENT = new Date($('#datepicker').val());
+
+    let calculator = new Calculator(AMOUNT_OF_CONTRIBUTION, YEAR_RATE ,TERM_OF_PAYMENT);
+    calculator.numberOfMonth();
+}
+
+//     // let daysCounter = (date) => Math.ceil(Math.abs(new Date(date).getTime() - new Date().getTime()) / (1000 * 3600 * 24));
+//     // let numberOfDays = daysCounter(TERM_OF_PAYMENT);
+
+//     result = +AMOUNT_OF_CONTRIBUTION;
+
+//     for (let i = 0; i < getNumberOfMonth(); i++) {
+//         console.log(getNumberOfMonth());
+//         calcMonthRevenue(today.getMonth() + i, today.getFullYear());
+//         drawReportContent({
+//             date: getCurrentDate(i),
+//             among: AMOUNT_OF_CONTRIBUTION,
+//             percents: percents.toFixed(2),
+//             result: result.toFixed(2)
+//         });
+//     }
+
+   
+//     function getNumberOfMonth() {
+//         // console.log('object');
+//         if (termOfPaymentDate.getFullYear() == today.getFullYear() && termOfPaymentDate.getMonth() == today.getMonth()) {
+//             return 1 ;
+//         } else return termOfPaymentDate.getMonth() + 1 - today.getMonth() + 1;
+//     }
+
+//     // function calcMonthRevenue(m, y) {
+//     //     if (m == today.getMonth() + 1 && y == today.getFullYear()) {
+//     //         console.log(daysInMonth(m, y));
+//     //         for (let i = today.getDate(); i < daysInMonth(m, y); i++) {
+//     //             percents = getDayRate();
+//     //             result += percents;
+//     //         }
+//     //     } else {
+//     //         for (let i = 0; i < daysInMonth(m, y); i++) {
+//     //             percents = getDayRate();
+//     //             result += percents;
+//     //         }
+//     //     }
+//     // }
+
+//     function daysInMonth(m, y) {
+//         return m === 2 ? y & 3 || !(y % 25) && y & 15 ? 28 : 29 : 30 + (m + (m >> 3) & 1);
+//     }
+
+//     function getDayRate() {
+//         return ((result / 100) * +YEAR_RATE) / 365;
+//     }
+
+   
+
+//     function getCurrentDate(shift) {
+//         if (termOfPaymentDate.getMonth() == today.getMonth()) {
+//             return `${termOfPaymentDate.getDate()}/${termOfPaymentDate.getMonth() + 1}/${termOfPaymentDate.getFullYear()}`;
+//         } else {
+//             let date = new Date(today.getFullYear(), today.getMonth() + 1 + shift, today.getDate());
+//             console.log(date);
+//             return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+//         }
+//     }
+
+
+// function drawReportContent(data) {
+//     console.log(data);
+//     $('.table-content').append(`
+//    <tr>
+//         <td>${data.date}</td>
+//         <td>${data.among}</td>
+//         <td>${data.percents}</td>
+//         <td>${data.result}</td>
+//    </tr>`);
+
+// }
